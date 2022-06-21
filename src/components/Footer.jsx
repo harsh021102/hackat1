68364af1f40e80fb07b720f63c7aa4b22e53ccf5
harsh021102 <harsh021102@gmail.com>
2022-06-22 02:36:37 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { BsGithub,BsInstagram,BsLinkedin} from "react-icons/bs";
 const Footer = () => {
   return (
     <div className='flex bg-dark-black h-full justify-center items-center flex-col gap-4'>
-        <h1 className='text-2xl text-white mt-8'>Created by <span className='text-light-blue font-bold'>Harsh Pratap Singh</span></h1>
+        <h1 className='text-xl sm:text-2xl text-white mt-8'>Created by <span className='text-light-blue font-bold'>Harsh Pratap Singh</span></h1>
         <div className='flex text-4xl gap-4 cursor-pointer mb-8 text-white'>
             <button><a href='https://github.com/harsh021102' rel="noopener"><BsGithub/></a></button>
             <button><a href='https://www.instagram.com/h_.a._.r._.s._h/' rel="noopener"><BsInstagram/></a></button>
